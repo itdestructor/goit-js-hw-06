1f01650ee2e0d978ba229c25cc595ef7e3086a05
Tikhon Tikhonov <ittikhon1995@gmail.com>
2022-09-01 19:34:14 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,36 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+let divAmountForm = document.querySelector('input')
+console.log(divAmountForm);
+let createBtn = document.querySelectorAll('button')[0]
+let deleteBtn = document.querySelectorAll('button')[1]
+let divBox = document.getElementById('boxes')
+
+let firstWidth = 30
+let firstHeight = 30
+
+createBtn.addEventListener('click',() => {
+
+
+  for(let i = 0;i < divAmountForm.value;i++){
+    let newBox = document.createElement('div');
+    newBox.style.width = firstWidth + 'px'
+    newBox.style.height = firstHeight + 'px'
+    newBox.style.backgroundColor = getRandomHexColor()
+    divBox.append(newBox)
+
+    firstHeight += 10
+    firstWidth += 10
+  
+  }
+
+  firstHeight = 30
+  firstWidth = 30
+})
+
+deleteBtn.addEventListener('click',() =>{
+  divBox.innerHTML = ''
+})
